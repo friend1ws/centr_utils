767149e6bcfa16a6b9b97480b982fd867c33daa4
friend1ws <friend1ws@gmail.com>
2020-09-26 07:30:23 +0000

    modified process_alpha_curated.sh

--- db/process_alpha_curated.sh
+++ db/process_alpha_curated.sh
@@ -4,4 +4,5 @@ python3 cycle.py alpha_curated.fa > alpha_curated_ext.fa
 
 centr_utils split_to_monomer alpha_curated_ext.fa alpha_curated_ext_monomer.fa ../example/alpha.hmm ../example/alpha.rc.hmm 
 
+python3 change_label.py alpha_curated_ext_monomer.fa HOR.list.txt > alpha_curated_proc.fa
 
